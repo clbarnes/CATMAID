2689c22c1d90878a10b153626de9d05bf4b224d3
Chris Barnes <barnesc@janelia.hhmi.org>
2017-11-28 17:08:09 -0500

    Tile: Allow viewing of HDF5 stacks

--- django/applications/catmaid/control/tile.py
+++ django/applications/catmaid/control/tile.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, division
 
 import os
 from contextlib import closing
@@ -7,7 +7,7 @@ import logging
 import numpy as np
 import base64
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from catmaid.models import UserRole, TILE_SOURCE_TYPES
 from catmaid.control.common import ConfigurationError
@@ -49,11 +49,13 @@ def get_tile(request, project_id=None, stack_id=None):
     z = int(request.GET.get('z', '0'))
     col = request.GET.get('col', 'y')
     row = request.GET.get('row', 'x')
-    file_extension = request.GET.get('file_extension', 'png')
+    file_extension = request.GET.get('file_extension', 'hdf5')
     basename = request.GET.get('basename', 'raw')
 
     # need to know the stack name
-    fpath=os.path.join( settings.HDF5_STORAGE_PATH, '{0}_{1}_{2}.hdf'.format( project_id, stack_id, basename ) )
+    # fpath=os.path.join( settings.HDF5_STORAGE_PATH, '{0}_{1}_{2}.hdf'.format( project_id, stack_id, basename ) )
+    outer_name, inner_path = basename.split('__')
+    fpath=os.path.join(settings.HDF5_STORAGE_PATH, '{}_{}.{}'.format(project_id, outer_name, file_extension))
 
     if not os.path.exists( fpath ):
         data=np.zeros( (height, width) )
@@ -64,24 +66,36 @@ def get_tile(request, project_id=None, stack_id=None):
         # return HttpResponse(json.dumps({'error': 'HDF5 file does not exists: {0}'.format(fpath)}))
 
     with closing(h5py.File(fpath, 'r')) as hfile:
-        #import math
-        #zoomlevel = math.log(int(scale), 2)
-        hdfpath = '/' + str(int(scale)) + '/' + str(z) + '/data'
-        if not str(int(scale)) in hfile['/'].keys():
-            data=np.zeros( (height, width) )
-            pilImage = Image.frombuffer('RGBA',(width,height),data,'raw','L',0,1)
-            response = HttpResponse(content_type="image/png")
-            pilImage.save(response, "PNG")
-            return response
-            # return HttpResponse(json.dumps({'error': 'HDF5 file does not contain scale: {0}'.format(str(int(scale)))}))
-        image_data=hfile[hdfpath]
-        data=image_data[y:y+height,x:x+width]
-        pilImage = Image.frombuffer('RGBA',(width,height),data,'raw','L',0,1)
-        response = HttpResponse(content_type="image/png")
-        pilImage.save(response, "PNG")
-        return response
+        image_data = hfile[inner_path]
+        data = np.array(image_data[z, y:y + height, x:x + width])
 
+    intmax = np.iinfo(np.uint8).max
+    img_arr = np.ones((height, width, 4), dtype=np.uint8)*intmax
+
+    if 'label' in inner_path:
+        # assume XY data, int>32
+        # truncate data to make low-fidelity probably-not-unique labels
+        uint8_data = data.view(np.uint8)
+        stacked_uint8_data = uint8_data.reshape(data.shape + (data.dtype.itemsize, ))
+        depth = min(3, data.dtype.itemsize)
+        truncated_uint8_data = stacked_uint8_data[:, :, :depth]
+        # todo: label mapping?
+        img_arr[:, :, :depth] = truncated_uint8_data
+    elif np.issubdtype(data.dtype, float):
+        # assume XYC (RGB) data between 0 and 1
+        img_arr[:, :, :3] = data[:, :, :3] * intmax
+
+    # pil_image = Image.fromarray(truncated_uint8_data, mode='RGB')
+    pil_image = Image.fromarray(img_arr, mode='RGBA')
+
+    # pil_image = Image.frombuffer('RGBA', (width, height), img_arr, 'raw', 'RGBA', 0, 1)
+    # np.save(os.path.join(settings.HDF5_STORAGE_PATH, '{}-{}-{}_split.npy'.format(x, y, z)), img_arr)
+    # pil_image.save(os.path.join(settings.HDF5_STORAGE_PATH, '{}-{}-{}.png'.format(x, y, z)))
+    response = HttpResponse(content_type="image/png")
+    response['access-control-allow-origin'] = '*'
+    pil_image.save(response, "PNG")
     return response
+
 
 @requires_user_role([UserRole.Annotate])
 def put_tile(request, project_id=None, stack_id=None):
